1ae48a8d8e4168bf61856278b26f25d3d52c64e0
samara <swondimu10@gmail.com>
2022-10-05 13:14:47 -0400

    added tests for map move

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -4,6 +4,13 @@ import junit.framework.*;
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    return null;
+    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    PacMan pacman = frame.addPacMan(new Location(9, 11));
+
+    assertEquals(map.move("pacman", new Location (2,4), Map.Type.PACMAN), true);
+
+    Ghost ghost = frame.addGhost(new Location(2, 4), "name", Color.red);
+
+    assertEquals(map.move("pacman", new Location (2,4), Map.Type.PACMAN), false);
   }
 }
