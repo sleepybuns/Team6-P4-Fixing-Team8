1a398ee48f07d48336216bfd4a4af94b95607a31
Darian_Maitland <DMaitland1999@gmail.com>
2022-10-05 17:22:13 -0400

    added test for is_ghost_in_range

--- Projects/P3/tests/TestGhostInRange.java
+++ Projects/P3/tests/TestGhostInRange.java
@@ -4,6 +4,11 @@ import junit.framework.*;
 public class TestGhostInRange extends TestCase {
 
   public void testGhostInRange() throws FileNotFoundException {
-    return null;
+    Frame frame = new MainFrame();
+
+    PacMan p = frame.addPacMan(new Location(6,4));
+    Ghost g = frame.addGhost(new Location(6,3), "Pinky", Color.pink);
+
+    assertTrue(p.is_ghost_in_range()==True);
   }
 }
